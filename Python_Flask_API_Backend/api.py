4e81371490e0134fbefbeac8a47da61b3d91d650
sete39 <saif8999056@gmail.com>
2020-06-24 21:03:40 +0400

    created a combined dictionary for all required info for the front end

--- Python_Flask_API_Backend/api.py
+++ Python_Flask_API_Backend/api.py
@@ -4,7 +4,7 @@ import pickle
 from riotwatcher import LolWatcher, TftWatcher, ApiError
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
-RIOTAPIKEY = 'RGAPI-a20656f5-0dcc-4c0f-b845-9656dd64cb3e'
+RIOTAPIKEY = 'RGAPI-1e83147a-a493-498c-8c41-6cc3f7754279'
 lol_watcher = LolWatcher(RIOTAPIKEY)
 tft_watcher = TftWatcher(RIOTAPIKEY)
 serverMap = {
@@ -12,42 +12,15 @@ serverMap = {
     'eun1': 'europe',
     'na1': 'AMERICAS',
 }
-# Create some test data for our catalog in the form of a list of dictionaries.
-# books = [
-#     {'id': 0,
-#      'title': 'A Fire Upon the Deep',
-#      'author': 'Vernor Vinge',
-#      'first_sentence': 'The coldsleep itself was dreamless.',
-#      'year_published': '1992'},
-#     {'id': 1,
-#      'title': 'The Ones Who Walk Away From Omelas',
-#      'author': 'Ursula K. Le Guin',
-#      'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-#      'published': '1973'},
-#     {'id': 2,
-#      'title': 'Dhalgren',
-#      'author': 'Samuel R. Delany',
-#      'first_sentence': 'to wound the autumnal city.',
-#      'published': '1975'}
-# ]
- 
-# @app.route('/', methods=['GET'])
-# def home():
-#     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-# @app.route('/api/v1/resources/books/all', methods=['GET'])
-# def api_all():
-#     return flask.jsonify(books)
 
-# @app.route('/api/v1/resources/books', methods=['GET'])
-# def api_id():
-#     if 'id' in flask.request.args:
-#         id = int(flask.request.args['id'])
-#     else:
-#         return "Error: no id field provided. Please specify an id."
+def changeSummonerInfo(summoner):
+    profileIconURL = 'https://cdn.communitydragon.org/10.12.1/profile-icon/' + str(summoner['profileIconId'])
+    summoner['profileIconURL'] = profileIconURL
+    for key in ['accountId', 'id', 'profileIconId', 'puuid', 'revisionDate']:
+        summoner.pop(key)
+    return summoner
 
-#     result = books[id]
-#     return flask.jsonify(result)
 @app.route('/v1/lol')
 def api_summoner_stats():
     if all(x in ['name', 'region'] for x in flask.request.args):
@@ -59,7 +32,7 @@ def api_summoner_stats():
     summonerInfo = lol_watcher.league.by_summoner(region, summoner['id'])
     summonerMatches = lol_watcher.match.matchlist_by_account(region, summoner['accountId'])
     firstMatch = lol_watcher.match.by_id(region, 4673926634)
-    json = flask.jsonify(firstMatch)
+    json = flask.jsonify(summoner)
     return json
 
 @app.route('/v1/tft')
@@ -70,18 +43,24 @@ def api_tft_stats():
     else:
         return "Error: no name field provided. Please specify a name."
     summoner = tft_watcher.summoner.by_name(region, name)
-    #summonerInfo = tft_watcher.league.by_summoner(region, summoner['id'])z1
+    #summonerInfo = tft_watcher.league.by_summoner(region, summoner['id'])
     summonerMatchesID = tft_watcher.match.by_puuid(serverMap[region], summoner['puuid'])
     matchList = []
     for matchID in summonerMatchesID:
         match = tft_watcher.match.by_id(serverMap[region], matchID)
         matchList.append(match)
-   # matchDF = matchDF.T
-    with open('data.pickle', 'wb') as f:
-    # Pickle the 'data' dictionary using the highest protocol available.
-        pickle.dump(matchList, f, pickle.HIGHEST_PROTOCOL)
-    print(matchList)
-    json = flask.jsonify(summonerMatchesID)
+    for i, match in enumerate(matchList):
+        for j, participant in enumerate(match['info']['participants']):
+            matchList[i]['info']['participant' + str(j)] = participant
+        matchList[i]['info'].pop('participants')
+        matchList[i]['metadata'].update(matchList[i]['info'])
+        matchList[i].pop('info')
+    summoner = changeSummonerInfo(summoner)
+    combinedDict = { # combining the match list and the summoner info into one dictionary
+        'matchList': matchList,
+        'summonerInfo': summoner
+    }
+    json = flask.jsonify(combinedDict)
     return json
 
 app.run()
