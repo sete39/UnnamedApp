90c7bc42f33dd8fbcaadd9418e988b2696a7a74f
sete39 <saif8999056@gmail.com>
2020-07-03 00:52:46 +0400

    created changeSummonerInfo function

--- Python_Flask_API_Backend/api.py
+++ Python_Flask_API_Backend/api.py
@@ -4,7 +4,7 @@ import pickle
 from riotwatcher import LolWatcher, TftWatcher, ApiError
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
-RIOTAPIKEY = 'RGAPI-1e83147a-a493-498c-8c41-6cc3f7754279'
+RIOTAPIKEY = 'RGAPI-6ada281d-aa04-4bc2-a99f-c4ba21f74570'
 lol_watcher = LolWatcher(RIOTAPIKEY)
 tft_watcher = TftWatcher(RIOTAPIKEY)
 serverMap = {
@@ -12,14 +12,20 @@ serverMap = {
     'eun1': 'europe',
     'na1': 'AMERICAS',
 }
+PATCHVERSION = '10.12.1'
 
-
-def changeSummonerInfo(summoner):
-    profileIconURL = 'https://cdn.communitydragon.org/10.12.1/profile-icon/' + str(summoner['profileIconId'])
+def changeSummonerInfo(summoner, summonerRankedInfo):
+    profileIconURL = 'https://cdn.communitydragon.org/' + PATCHVERSION + '/profile-icon/' + str(summoner['profileIconId'])
     summoner['profileIconURL'] = profileIconURL
     for key in ['accountId', 'id', 'profileIconId', 'puuid', 'revisionDate']:
         summoner.pop(key)
+    for i in range(len(summonerRankedInfo)):
+        summoner['rankedInfo' + str(i)] = summonerRankedInfo[i]['tier'] + summonerRankedInfo[i]['rank']
+        summoner['rankedType' + str(i)] = summonerRankedInfo[i]['queueType']
     return summoner
+
+def getTftChampIcon(characterId):
+    return 0
 
 @app.route('/v1/lol')
 def api_summoner_stats():
@@ -32,7 +38,7 @@ def api_summoner_stats():
     summonerInfo = lol_watcher.league.by_summoner(region, summoner['id'])
     summonerMatches = lol_watcher.match.matchlist_by_account(region, summoner['accountId'])
     firstMatch = lol_watcher.match.by_id(region, 4673926634)
-    json = flask.jsonify(summoner)
+    json = flask.jsonify(firstMatch)
     return json
 
 @app.route('/v1/tft')
@@ -43,10 +49,10 @@ def api_tft_stats():
     else:
         return "Error: no name field provided. Please specify a name."
     summoner = tft_watcher.summoner.by_name(region, name)
-    #summonerInfo = tft_watcher.league.by_summoner(region, summoner['id'])
+    summonerRankedInfo = tft_watcher.league.by_summoner(region, summoner['id'])
     summonerMatchesID = tft_watcher.match.by_puuid(serverMap[region], summoner['puuid'])
     matchList = []
-    for matchID in summonerMatchesID:
+    for matchID in summonerMatchesID: 
         match = tft_watcher.match.by_id(serverMap[region], matchID)
         matchList.append(match)
     for i, match in enumerate(matchList):
@@ -55,7 +61,7 @@ def api_tft_stats():
         matchList[i]['info'].pop('participants')
         matchList[i]['metadata'].update(matchList[i]['info'])
         matchList[i].pop('info')
-    summoner = changeSummonerInfo(summoner)
+    summoner = changeSummonerInfo(summoner, summonerRankedInfo)
     combinedDict = { # combining the match list and the summoner info into one dictionary
         'matchList': matchList,
         'summonerInfo': summoner
@@ -63,4 +69,4 @@ def api_tft_stats():
     json = flask.jsonify(combinedDict)
     return json
 
-app.run()
+app.run() 
